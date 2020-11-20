61b072ac73d8446b7245c5e58131876a83cdc2e0
EmerisScala <emeris.hernandez@e-evolution.com>
2020-11-20 13:15:37 -0600

    Migrate Image path resource function

--- src/ADempiere/shared/utils/types.d.ts
+++ src/ADempiere/shared/utils/types.d.ts
@@ -1,17 +1,31 @@
 interface IConfigData {
-    server : { 
-        host: string,
+    server: {
+        host: string
         port: number
-    },
+    }
     adempiere: {
         api: {
-            url: string,
+            url: string
             service: string
-        },
+        }
         images: {
-            url: string,
+            url: string
             service: string
         }
     }
 }
-  
+
+// Resource types
+
+export interface ImagePathData {
+    url: string
+    urn: string
+    uri: string
+}
+
+export interface IRequestImageData {
+    file: string
+    width: number
+    height: number
+    operation: string
+}
