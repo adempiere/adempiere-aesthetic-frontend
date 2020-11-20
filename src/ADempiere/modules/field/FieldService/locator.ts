848dcc6e35ab5b87f31aba0536181568ab8959a7
EmerisScala <emeris.hernandez@e-evolution.com>
2020-11-20 13:11:11 -0600

    Migrate field api service module

---
+++ src/ADempiere/modules/field/FieldService/locator.ts
@@ -0,0 +1,21 @@
+import {
+  IEntityListData,
+  requestListEntities
+} from '@/ADempiere/modules/persistence';
+
+const tableName = 'M_Locator';
+
+interface ILocatorListParams {
+    warehouseId: number
+}
+
+export function requestLocatorList(
+  data: ILocatorListParams
+): Promise<IEntityListData> {
+  const { warehouseId } = data;
+
+  return requestListEntities({
+    tableName,
+    whereClause: `M_Warehouse_ID = ${warehouseId}`
+  });
+}
