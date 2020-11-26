220095db3b67ddcc7957125508468c52a0fbc544
EmerisScala <emeris.hernandez@e-evolution.com>
2020-11-26 15:03:03 -0600

    Add Business Partner store to Vuex Persist

--- src/ADempiere/shared/plugins/vuex-persist.ts
+++ src/ADempiere/shared/plugins/vuex-persist.ts
@@ -1,5 +1,8 @@
 import VuexPersistence from 'vuex-persist'
 export default new VuexPersistence({
   storage: window.localStorage,
-  modules: ['coreModule']
+  modules: [
+    'coreModule',
+    'businessPartnerModule'
+  ]
 })
