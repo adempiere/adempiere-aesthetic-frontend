26f50cbc8bc8a5dd86184e211ee5a09a7d3a81bc
EmerisScala <emeris.hernandez@e-evolution.com>
2020-12-21 14:26:42 -0600

    Add Point Of Sales Vuex Module to Vuex Persist

--- src/ADempiere/shared/plugins/vuex-persist.ts
+++ src/ADempiere/shared/plugins/vuex-persist.ts
@@ -21,6 +21,7 @@ export default new VuexPersistence({
     'containerInfoModule',
     'reportModule',
     'fieldValueModule',
-    'processModule'
+    'processModule',
+    'pointOfSalesModule'
   ]
 })
