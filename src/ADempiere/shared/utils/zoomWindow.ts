cb9123f228fece0154bdc9b894e83784657198b1
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-04 13:14:51 -0600

    Migrate zoom utils

---
+++ src/ADempiere/shared/utils/zoomWindow.ts
@@ -0,0 +1,37 @@
+export interface IZoomWindowRoute {
+    [key: string]: {
+        uuid: string
+        action?: any
+        tabParent: string
+        tabChild?: any
+    }
+}
+
+const ROUTES: IZoomWindowRoute = {
+  PRINT_FORMAT_SETUP_WINDOW: {
+    uuid: '8e5131dc-fb40-11e8-a479-7a0060f0aa01',
+    action: undefined,
+    tabParent: '0',
+    tabChild: undefined
+  },
+  REQUEST_WINDOW: {
+    uuid: '8e510176-fb40-11e8-a479-7a0060f0aa01',
+    action: undefined,
+    tabParent: '0',
+    tabChild: undefined
+  },
+  WORKFLOW_WINDOW: {
+    uuid: '8e5168e6-fb40-11e8-a479-7a0060f0aa01',
+    action: undefined,
+    tabParent: '0',
+    tabChild: undefined
+  },
+  DOCUMENTS_WINDOW: {
+    uuid: '8e50d7c8-fb40-11e8-a479-7a0060f0aa01',
+    action: undefined,
+    tabParent: '0',
+    tabChild: undefined
+  }
+}
+
+export default ROUTES
