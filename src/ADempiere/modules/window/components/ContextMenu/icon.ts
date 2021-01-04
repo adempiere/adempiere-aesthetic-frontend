eaea7aa5f2d8a94ae013a0373b1b6840405410db
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-04 13:18:11 -0600

    Migrate Icon list from Context Menu Component

---
+++ src/ADempiere/modules/window/components/ContextMenu/icon.ts
@@ -0,0 +1,36 @@
+export type ContextMenuIconType = {
+  type: string
+  icon: string
+}
+
+export const icon: ContextMenuIconType[] = [
+  {
+    type: 'window',
+    icon: 'tab'
+  },
+  {
+    type: 'report',
+    icon: 'skill'
+  },
+  {
+    type: 'task',
+    icon: 'size'
+  },
+  {
+    type: 'process',
+    icon: 'component'
+  },
+  {
+    type: 'browser',
+    icon: 'search'
+  },
+  {
+    type: 'workflow',
+    icon: 'example'
+  },
+  {
+    type: 'form',
+    icon: 'form'
+  }
+
+]
