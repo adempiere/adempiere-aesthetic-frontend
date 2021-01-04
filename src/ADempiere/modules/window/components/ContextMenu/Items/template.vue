c6ab76a7e4bde44a00d8940a23f86d719fc1337f
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-04 14:12:42 -0600

    Migrate Items component from Context Menu Component

---
+++ src/ADempiere/modules/window/components/ContextMenu/Items/template.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-submenu
+        v-if="item.meta.type === 'summary'"
+        key="is-summary"
+        :index="item.meta.title"
+        popper-append-to-body
+    >
+        <template slot="title">
+            <svg-icon v-if="isMobile" icon-class="nested" />
+            {{ item.meta.title }}
+        </template>
+        <item
+            v-for="(child, subKey) in item.children"
+            :key="subKey"
+            :item="child"
+        >
+            {{ child.meta.title }}
+        </item>
+    </el-submenu>
+    <el-menu-item
+        v-else
+        v-show="item.meta.uuid !== $route.meta.uuid"
+        key="not-summary"
+        :index="item.meta.uuid"
+        @click="handleClick(item)"
+    >
+        <svg-icon v-if="isMobile" :icon-class="classIconMenuRight" />
+        {{ item.meta.title }}
+    </el-menu-item>
+</template>
+
+<style>
+.scroll {
+    max-height: 400px;
+}
+</style>
