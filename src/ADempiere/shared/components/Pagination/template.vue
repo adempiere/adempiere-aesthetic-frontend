52f41b62a37f6abd04ce6a04182206d5e2180477
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-05 16:12:53 -0600

    Migrate Pagination component

---
+++ src/ADempiere/shared/components/Pagination/template.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-footer style="height: 30px;">
+    <div style="float: right;">
+      <el-pagination
+        small
+        layout="slot, total, prev, pager, next"
+        :current-page="currentPage"
+        :page-size="pageSize"
+        :total="total"
+        @current-change="handleChangePage"
+      >
+        <template v-slot>
+          <span v-if="isSelection">
+            {{ $t('table.dataTable.selected') }}: {{ selection }} / <!-- show total records -->
+          </span>
+        </template>
+      </el-pagination>
+    </div>
+  </el-footer>
+</template>
