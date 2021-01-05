8d9f722ccf4a60e56cb87527d84fae46dad84ae9
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-05 11:36:10 -0600

    Migrate RecentItems component from Dashboard Component

---
+++ src/ADempiere/modules/dashboard/components/Dashboard/RecentItems/template.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-card class="box-card" :body-style="{padding: '0px'}" shadow="never">
+    <div class="recent-items">
+      <el-table :data="dataResult" max-height="455" @row-click="handleClick">
+        <el-table-column width="40">
+          <template slot-scope="{row}">
+            <svg-icon :icon-class="row.icon" class="icon-window" />
+          </template>
+        </el-table-column>
+        <el-table-column>
+          <template slot="header" slot-scope="scope" class="clearfix">
+            <el-input
+              v-model="search"
+              size="mini"
+              :metadata="scope"
+              :placeholder="$t('table.dataTable.search')"
+            >
+              <svg-icon slot="prefix" icon-class="search" />
+            </el-input>
+          </template>
+          <template slot-scope="{row}">
+            <span>{{ row.displayName }}</span>
+            <el-tag class="action-tag">
+              {{ $t(`views.${row.action}`) }}
+            </el-tag>
+            <br>
+            <span class="time">
+              {{ translateDate(row.updated) }}
+            </span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </el-card>
+</template>
+
+<style scoped>
+  .search_recent {
+    width: 50% !important;
+    float: right;
+  }
+  .header {
+    padding-bottom: 10px;
+  }
+  .recent-items {
+    height: 455px;
+    overflow: auto;
+  }
+  .time {
+    float: left;
+    font-size: 11px;
+    color: #999;
+  }
+  .card-box {
+    cursor: pointer;
+  }
+  .card-content {
+    font-size: 15px;
+  }
+  .icon-window {
+    font-size: x-large;
+    color: #36a3f7;
+  }
+  .action-tag {
+    float: right;
+  }
+</style>
