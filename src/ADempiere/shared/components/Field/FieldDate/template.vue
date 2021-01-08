1800ed5ed80138c5341508eaf38d8638d534e0dc
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-08 14:54:53 -0600

    Migrate FieldDate component from Field component

---
+++ src/ADempiere/shared/components/Field/FieldDate/template.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-date-picker
+    :ref="metadata.columnName"
+    v-model="value"
+    :format="formatView"
+    :value-format="formatSend"
+    :type="typePicker"
+    range-separator="-"
+    :placeholder="metadata.help"
+    :start-placeholder="$t('components.dateStartPlaceholder')"
+    :end-placeholder="$t('components.dateEndPlaceholder')"
+    unlink-panels
+    :class="cssClassStyle"
+    :readonly="Boolean(metadata.readonly)"
+    :disabled="isDisabled"
+    :picker-options="pickerOptions"
+    @change="preHandleChange"
+    @blur="focusLost"
+    @focus="focusGained"
+    @keydown.native="keyPressed"
+    @keyup.native="keyReleased"
+  />
+</template>
+
+<style scoped>
+  .custom-field-date {
+    width: 100% !important;
+  }
+</style>
