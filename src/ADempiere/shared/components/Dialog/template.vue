6f54332354eee075b56c17bc050bd2b4bcb8c3ab
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-08 19:08:38 -0600

    Migrate Dialog component

---
+++ src/ADempiere/shared/components/Dialog/template.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-dialog
+    :title="modalMetadata.name"
+    :visible="isVisibleDialog"
+    show-close
+    :before-close="closeDialog"
+    :width="width + '%'"
+    top="5vh"
+    close-on-press-escape
+    close-on-click-modal
+  >
+    {{ modalMetadata.description }}<br><br>
+    <sequence-order
+      v-if="modalMetadata.isSortTab"
+      key="order"
+      :parent-uuid="parentUuid"
+      :container-uuid="modalMetadata.uuid"
+      :order="modalMetadata.sortOrderColumnName"
+      :included="modalMetadata.sortYesNoColumnName"
+      :identifiers-list="modalMetadata.identifierColumns"
+      :key-column="modalMetadata.keyColumn"
+    />
+    <template v-else>
+      <main-panel
+        v-if="!isEmptyValue(modalMetadata.uuid)"
+        key="main-panel"
+        :parent-uuid="parentUuid"
+        :container-uuid="modalMetadata.uuid"
+        :metadata="modalMetadata"
+        :panel-type="modalMetadata.panelType"
+      />
+    </template>
+    <span slot="footer" class="dialog-footer">
+      <el-button
+        @click="closeDialog"
+      >
+        {{ $t('components.dialogCancelButton') }}
+      </el-button>
+      <el-button
+        type="primary"
+        @click="runAction(modalMetadata)"
+      >
+        {{ $t('components.dialogConfirmButton') }}
+      </el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<style>
+  .el-dialog__body {
+    padding: 10px 20px;
+    max-height: 65vh;
+    overflow: auto;
+  }
+</style>
