28cba188d40543d06b750cb535470e43a0450180
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-11 19:32:40 -0600

    Migrate fieldListConvertAmountCollection from ConvertAmount component

---
+++ src/ADempiere/shared/components/Form/VPOS/Collection/ConvertAmount/fieldListConvertAmountCollection.ts
@@ -0,0 +1,21 @@
+import { IFieldLocation } from '@/ADempiere/shared/components/Field/FieldLocation/fieldList'
+
+const tableName = 'C_Order'
+const fieldListConvertAmountCollection: IFieldLocation[] = [
+  // Currency
+  {
+    tableName,
+    columnName: 'C_Currency_ID',
+    isFromDictionary: true,
+    overwriteDefinition: {
+      size: 24,
+      handleActionKeyPerformed: true,
+      handleActionPerformed: true,
+      validationCode: 'C_Currency.C_Currency_ID = 100',
+      isActiveLogics: true,
+      isMandatory: true
+    }
+  }
+]
+
+export default fieldListConvertAmountCollection
