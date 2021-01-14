611aa9d755380a12f972ba8baccce7ca5994ac48
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-14 10:08:17 -0600

    Migrate Process View

---
+++ src/ADempiere/modules/process/views/Process/index.ts
@@ -0,0 +1,65 @@
+import Template from './template.vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import MainPanel from '@/ADempiere/shared/components/Panel'
+import ContextMenu from '../../../window/components/ContextMenu'
+import { PanelContextType } from '@/ADempiere/shared/utils/DictionaryUtils/ContextMenuType'
+import { SettingsModule } from '@/store/modules/settings'
+import { Namespaces } from '@/ADempiere/shared/utils/types'
+import { IPanelDataExtended } from '../../../dictionary'
+
+@Component({
+  name: 'ProcessView',
+  mixins: [Template],
+  components: {
+    MainPanel,
+    ContextMenu
+  }
+})
+export default class ProcessView extends Vue {
+    @Prop({ type: Boolean, default: false }) isEdit = false
+    public processMetadata: Partial<IPanelDataExtended> = {}
+    public processUuid: string = this.$route.meta.uuid
+    public isLoadedMetadata = false
+    public panelType: PanelContextType = PanelContextType.Process
+
+    // Computed Properties
+    get showContextMenu(): boolean | undefined {
+      return SettingsModule.showContextMenu
+    }
+
+    get getterProcess(): IPanelDataExtended | undefined {
+      return this.$store.getters[Namespaces.Panel + '/' + 'getPanel'](this.processUuid)
+    }
+
+    get processTitle(): string {
+      return this.processMetadata.name || this.$route.meta.title
+    }
+
+    // Methods
+    getProcess(): void {
+      const process: IPanelDataExtended | undefined = this.getterProcess
+      if (process) {
+        this.processMetadata = process
+        this.isLoadedMetadata = true
+      } else {
+        this.$store.dispatch('getPanelAndFields', {
+          containerUuid: this.processUuid,
+          panelType: this.panelType,
+          routeToDelete: this.$route
+        }).then(processResponse => {
+          this.processMetadata = processResponse
+        }).finally(() => {
+          this.isLoadedMetadata = true
+        })
+      }
+    }
+
+    // Hooks
+    created() {
+      this.getProcess()
+      this.$store.dispatch('settings/changeSetting', {
+        key: 'showContextMenu',
+        value: true
+      })
+    }
+}
