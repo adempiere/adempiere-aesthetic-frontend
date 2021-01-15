b04f934677a263f8acfe0197c5d6a417026a4cfd
EmerisScala <emeris.hernandez@e-evolution.com>
2021-01-14 18:02:18 -0600

    Convert JSON input to IConfigData type

--- src/ADempiere/shared/utils/config.ts
+++ src/ADempiere/shared/utils/config.ts
@@ -2,5 +2,21 @@ import * as config from '@/../config/default.json'
 import { IConfigData } from './types'
 
 export function getConfig(): IConfigData {
-  return config
+  return {
+    adempiere: {
+      api: {
+        service: JSON.stringify(config.adempiere.api.service).replace(/['"]+/g, ''),
+        url: JSON.stringify(config.adempiere.api.url).replace(/['"]+/g, '')
+      },
+      images: {
+        service: JSON.stringify(config.adempiere.images.service).replace(/['"]+/g, ''),
+        url: JSON.stringify(config.adempiere.images.url).replace(/['"]+/g, '')
+      }
+    },
+    server: {
+      host: JSON.stringify(config.server.host).replace(/['"]+/g, ''),
+      port: Number(JSON.stringify(config.server.port).replace(/['"]+/g, ''))
+    }
+  }
+  // return <IConfigData>JSON.stringify(config)
 }
