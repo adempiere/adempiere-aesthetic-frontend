2f6de9b3acb423edc1e2efb88a38b88b63712dcc
EmerisScala <emeris.hernandez@e-evolution.com>
2021-02-25 13:40:14 -0600

    Add Mixin to Mixins decorator

--- src/ADempiere/modules/window/components/ContainerInfo/WorkflowLogs/index.ts
+++ src/ADempiere/modules/window/components/ContainerInfo/WorkflowLogs/index.ts
@@ -1,5 +1,5 @@
 import { Namespaces } from '@/ADempiere/shared/utils/types'
-import { Vue, Component } from 'vue-property-decorator'
+import { Component, Mixins } from 'vue-property-decorator'
 import MixinInfo from '../mixinInfo'
 import { IWorkflowProcessData } from '@/ADempiere/modules/window/WindowType'
 import Template from './template.vue'
@@ -8,7 +8,7 @@ import Template from './template.vue'
   name: 'WorkflowLogs',
   mixins: [Template, MixinInfo]
 })
-export default class WorkflowLogs extends Vue {
+export default class WorkflowLogs extends Mixins(MixinInfo) {
   // Computed properties
   get gettersListWorkflow(): IWorkflowProcessData[] {
     return this.$store.getters[
