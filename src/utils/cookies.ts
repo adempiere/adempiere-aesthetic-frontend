c52cf7f219d177c69246f1fc17ebf45d58beb399
EmerisScala <emeris.hernandez@e-evolution.com>
2021-03-01 19:58:23 -0600

    Fix SameSite Cookies warning in Firefox

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -3,19 +3,19 @@ import Cookies from 'js-cookie'
 // App
 const sidebarStatusKey = 'sidebar_status'
 export const getSidebarStatus = () => Cookies.get(sidebarStatusKey)
-export const setSidebarStatus = (sidebarStatus: string) => Cookies.set(sidebarStatusKey, sidebarStatus)
+export const setSidebarStatus = (sidebarStatus: string) => Cookies.set(sidebarStatusKey, sidebarStatus, { sameSite: 'Lax' })
 
 const languageKey = 'language'
 export const getLanguage = () => Cookies.get(languageKey)
-export const setLanguage = (language: string) => Cookies.set(languageKey, language)
+export const setLanguage = (language: string) => Cookies.set(languageKey, language, { sameSite: 'Lax' })
 
 const sizeKey = 'size'
 export const getSize = () => Cookies.get(sizeKey)
-export const setSize = (size: string) => Cookies.set(sizeKey, size)
+export const setSize = (size: string) => Cookies.set(sizeKey, size, { sameSite: 'Lax' })
 
 // User
 // const tokenKey = 'vue_typescript_admin_access_token'
 const tokenKey = 'sessionUuid'
 export const getToken = () => Cookies.get(tokenKey)
-export const setToken = (token: string) => Cookies.set(tokenKey, token)
+export const setToken = (token: string) => Cookies.set(tokenKey, token, { sameSite: 'Lax' })
 export const removeToken = () => Cookies.remove(tokenKey)
