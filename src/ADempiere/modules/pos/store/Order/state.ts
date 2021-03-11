9a8ff991cbb7c691c29ffb858afe9efb5bdf2ffa
EmerisScala <emeris.hernandez@e-evolution.com>
2021-03-11 14:53:34 -0600

    Expand POS Order initial state

--- src/ADempiere/modules/pos/store/Order/state.ts
+++ src/ADempiere/modules/pos/store/Order/state.ts
@@ -1,7 +1,40 @@
 import { OrderState } from '../../POSType'
 
 export const state: OrderState = {
-  order: undefined,
+  order: {
+    documentType: {
+      description: '',
+      name: '',
+      id: 0,
+      printName: '',
+      uuid: ''
+    },
+    documentStatus: {
+      description: '',
+      value: '',
+      name: ''
+    },
+    totalLines: 0,
+    grandTotal: 0,
+    salesRepresentative: {
+      name: '',
+      description: '',
+      id: 0,
+      uuid: ''
+    },
+    businessPartner: {
+      description: '',
+      duns: '',
+      id: 0,
+      lastName: '',
+      naics: '',
+      name: '',
+      taxId: '',
+      value: '',
+      uuid: ''
+    },
+    uuid: ''
+  },
   findOrder: undefined,
   listOrder: {
     isLoaded: false,
