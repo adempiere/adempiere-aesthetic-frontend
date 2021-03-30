9b8d7a653aa8942c27c6ef28626dcdd0b7a1e1b1
Emeris Hernandez <emerisortiz@gmail.com>
2021-03-30 10:42:27 -0600

    Remove old management of router

--- src/ADempiere/shared/store/index.ts
+++ src/ADempiere/shared/store/index.ts
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Vuex, { StoreOptions } from 'vuex'
 import vuexLocal from '@/ADempiere/shared/plugins/vuex-persist'
 import { getters } from './getters'
-import { mutations } from './mutations'
-import { actions } from './actions'
 //
 import { IRootState } from '@/store'
 import { modules } from './modules'
@@ -13,8 +11,6 @@ Vue.use(Vuex)
 const store: StoreOptions<IRootState> = {
   modules: modules,
   getters: getters,
-  mutations: mutations,
-  actions: actions,
   plugins: [vuexLocal.plugin]
 }
 
