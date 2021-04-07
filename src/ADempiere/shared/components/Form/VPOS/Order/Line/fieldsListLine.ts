3d8ed3141409a9bed6b49f422a30ad6dcc2485b3
Emeris Hernandez <emerisortiz@gmail.com>
2021-04-07 18:34:38 -0500

    Migrate FieldLine component

---
+++ src/ADempiere/shared/components/Form/VPOS/Order/Line/fieldsListLine.ts
@@ -0,0 +1,44 @@
+import { IFieldLocation } from '@/ADempiere/shared/components/Field/FieldLocation/fieldList'
+
+const fieldsList: IFieldLocation[] = [
+  {
+    elementColumnName: 'PriceEntered',
+    columnName: 'PriceEntered',
+    isFromDictionary: true,
+    overwriteDefinition: {
+      size: 24,
+      sequence: 8,
+      isReadOnly: true,
+      handleActionPerformed: true,
+      handleContentSelection: true,
+      handleActionKeyPerformed: true
+    }
+  },
+  {
+    elementColumnName: 'QtyEntered',
+    columnName: 'QtyEntered',
+    isFromDictionary: true,
+    overwriteDefinition: {
+      size: 24,
+      sequence: 9,
+      handleActionPerformed: true,
+      handleContentSelection: true,
+      handleActionKeyPerformed: true
+    }
+  },
+  {
+    elementColumnName: 'Discount',
+    columnName: 'Discount',
+    isFromDictionary: true,
+    overwriteDefinition: {
+      size: 24,
+      sequence: 10,
+      isReadOnly: true,
+      handleActionPerformed: true,
+      handleContentSelection: true,
+      handleActionKeyPerformed: true
+    }
+  }
+]
+
+export default fieldsList
