b0ab34823c6caa26f7ebd9169e4f58d9733fccb8
Emeris Hernandez <emerisortiz@gmail.com>
2021-04-20 18:47:40 -0500

    Fix ContextMenuDesktop

--- src/ADempiere/modules/window/components/ContextMenu/ContextMenuDesktop/index.ts
+++ src/ADempiere/modules/window/components/ContextMenu/ContextMenuDesktop/index.ts
@@ -1,4 +1,7 @@
+import { ProcessDefinitionAction } from '@/ADempiere/modules/dictionary'
+import VueI18n from 'vue-i18n'
 import { Component, Mixins, Vue } from 'vue-property-decorator'
+import { WindowTabAssociatedAction, WindowProcessAsociatedAction, WindowDefinitionAction, RecordAccessAction, IContextActionData } from '../../../WindowType'
 import MixinContextMenu from '../MixinContextMenu'
 import Template from './template.vue'
 
@@ -7,5 +10,122 @@ import Template from './template.vue'
   mixins: [MixinContextMenu, Template]
 })
 export default class ContextMenuDesktop extends Mixins(MixinContextMenu) {
-    private none: any = 'remove it'
+  private openedsMenu: string[] = ['actions']
+  // Computed properties
+  get isPanelTypeMobile() {
+    if (['process', 'report'].includes(this.$route.meta.type)) {
+      return true
+    }
+    return false
+  }
+
+  get isUndoAction(): boolean {
+    if (this.isWindow) {
+      if (!this.isWithRecord) {
+        return true
+      }
+    }
+    return false
+  }
+
+  get typeOfAction(): 'warning' | 'default' {
+    if (this.isUndoAction) {
+      return 'warning'
+    }
+    return 'default'
+  }
+
+  get isPlain(): boolean {
+    if (this.isUndoAction) {
+      return true
+    }
+    return false
+  }
+
+  get defaultActionToRun(): IContextActionData {
+    if (this.isUndoAction) {
+      return this.actions[2]
+    }
+    return this.actions[0]
+  }
+
+  get defaultActionName(): VueI18n.TranslateResult {
+    if (this.isWindow) {
+      if (this.isWithRecord) {
+        return this.$t('window.newRecord')
+      }
+      return this.$t('data.undo')
+    }
+    return this.$t('components.RunProcess')
+  }
+
+  get iconDefault(): 'component' | 'skill' {
+    if (this.isPanelTypeMobile) {
+      return 'component'
+    }
+    return 'skill'
+  }
+
+  // Methods
+  clickRelation(item: { name: string | undefined }) {
+    this.$router.push({
+      name: item.name,
+      query: {
+        tabParent: (0).toString()
+      }
+    }, () => {})
+  }
+
+  clickRunAction(action: string | IContextActionData) {
+    if (action === 'refreshData') {
+      this.refreshData()
+    } else {
+      if (typeof action !== 'string') {
+        this.runAction(action)
+      }
+    }
+  }
+
+  clickReferences(reference: any) {
+    this.openReference(reference)
+  }
+
+  iconAction(action: IContextActionData) {
+    let icon
+    if (action.type === 'dataAction') {
+      switch (action.action) {
+        case 'setDefaultValues':
+          icon = 'el-icon-news'
+          break
+        case 'deleteEntity':
+          icon = 'el-icon-delete'
+          break
+        case 'undoModifyData':
+          icon = 'el-icon-refresh-left'
+          break
+        case 'lockRecord':
+          icon = 'el-icon-lock'
+          break
+        case 'unlockRecord':
+          icon = 'el-icon-unlock'
+          break
+        case 'recordAccess':
+          icon = 'el-icon-c-scale-to-original'
+          break
+      }
+    } else if (action.type === 'process') {
+      icon = 'el-icon-setting'
+    } else {
+      icon = 'el-icon-setting'
+    }
+    return icon
+  }
+
+  styleLabelAction(value: any): string {
+    if (value) {
+      return 'font-size: 14px;margin-top: 0% !important;margin-left: 0px;margin-bottom: 10%;display: contents;'
+    } else {
+      return 'font-size: 14px;margin-top: 1.5% !important;margin-left: 2%;margin-bottom: 5%;display: contents;'
+    }
+  }
 }
